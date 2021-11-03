7d3f8fa64a00263da5cb89eb2dd686a8c49ae10b
ericdavidanderson <burrej1@gmail.com>
2021-11-03 16:03:30 -0700

    syntax fix

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -4,7 +4,7 @@ test("testing Engineer class construction"), () => {
     // it("should add a github username to a constructor"
     const testValue = "testUser"; 
         const username = new Engineer("Eric", 1, "github.com/ericdavidanderson/", testValue);
-        const obj = new Engineer(github);
+        // const obj = new Engineer(github);
 
         expect(username.gethub).toBe(testValue);
     };
