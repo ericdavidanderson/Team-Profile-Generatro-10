0dc2b81f3b0cd4081346750ca0e4660ee4a5aba9
ericdavidanderson <burrej1@gmail.com>
2021-11-03 16:17:37 -0700

    syntax line 3

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,12 +1,12 @@
 
 const Engineer = require("../lib/Engineer");
-test("testing Engineer class construction"), () => {
+test("testing Engineer class construction", () => {
     // it("should add a github username to a constructor"
-    const testValue = "testUser"; 
-        const username = new Engineer("Eric", 1, "github.com/ericdavidanderson/", testValue);
-        // const obj = new Engineer(github);
+    const testValue = "testUser";
+    const username = new Engineer("Eric", 1, "testing@test.net", testValue);
+// const obj = new Engineer(github);
 
-        expect(username.gethub).toBe(testValue);
-    };
+expect(username.github).toBe(testValue);
+};
 
-    
+
