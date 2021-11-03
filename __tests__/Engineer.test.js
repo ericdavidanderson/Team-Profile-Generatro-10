8c3a5ef7b3654f83b62a9a13cd3ae445fa86aa8d
ericdavidanderson <burrej1@gmail.com>
2021-11-03 01:53:47 -0700

    Constructors complete, 2 tests run

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,11 @@
+
+const Engineer = require("../lib/Engineer");
+describe("testing Engineer class construction", () => {
+    it("should add a github username to a constructor", () => {
+        var username = "github.com/ericdavidanderson/";
+        const obj = new Engineer(github);
+
+        expect(obj.github).toEqual(username);
+    });
+    }
+});
