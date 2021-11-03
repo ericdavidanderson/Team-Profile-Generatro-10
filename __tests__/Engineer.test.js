5336bdacaf156868dca6c0644120f25df2759536
ericdavidanderson <burrej1@gmail.com>
2021-11-03 16:02:05 -0700

    syntax fix

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,7 +2,7 @@
 const Engineer = require("../lib/Engineer");
 test("testing Engineer class construction"), () => {
     // it("should add a github username to a constructor"
-    const testValue = "testUser"; {
+    const testValue = "testUser"; 
         const username = new Engineer("Eric", 1, "github.com/ericdavidanderson/", testValue);
         const obj = new Engineer(github);
 
