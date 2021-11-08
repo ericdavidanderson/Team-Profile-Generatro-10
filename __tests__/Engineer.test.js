b8af88e1b44bfe6bac8080cbd8b34380ca1c1149
ericdavidanderson <burrej1@gmail.com>
2021-11-07 22:05:13 -0800

    template generator

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,6 +1,7 @@
+//remember to import employee
 
 const Engineer = require("../lib/Engineer");
-test("testing Engineer class construction", () => {
+test("testing ability to add github user ID to Engineer class", () => {
     // it("should add a github username to a constructor"
     const testValue = "testUser";
     const username = new Engineer("Eric", 1, "testing@test.net", testValue);
